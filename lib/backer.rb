7fe3563c53e18eedd63c8c73bac2830e5d8a8d53
Angelina Montanez <angelinamontanez80@gmail.com>
2019-03-24 00:57:52 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,5 +1,5 @@
 class Backer
-  attr_reader :backer 
+  attr_reader :backer
 bob = Backer.new("Bob")
 end
 
@@ -13,10 +13,10 @@ end
 
 bob = Backer.new("Bob")
 awesome_project = Project.new("This is an Awesome Project")
- 
+
 bob.back_project(awesome_project)
- 
+
 bob.backed_projects
 # => #<Project:0x000001018683d0 @title="This is an Awesome Project"...>
- 
+
 awesome_project.backers
