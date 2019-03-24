a52e705b1fe20408ca6c5110ab22d4de4351bf13
Angelina Montanez <angelinamontanez80@gmail.com>
2019-03-24 00:58:28 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -3,15 +3,6 @@ class Backer
 bob = Backer.new("Bob")
 end
 
-  describe 'Backer - ::new' do
-    it 'takes a name on initialization, accessible through an attribute reader' do
-      backer = Backer.new("Avi")
-      expect(backer.name).to eq("Avi")
-    end
-
-
-
-bob = Backer.new("Bob")
 awesome_project = Project.new("This is an Awesome Project")
 
 bob.back_project(awesome_project)
@@ -20,3 +11,12 @@ bob.backed_projects
 # => #<Project:0x000001018683d0 @title="This is an Awesome Project"...>
 
 awesome_project.backers
+  describe 'Backer - ::new' do
+    it 'takes a name on initialization, accessible through an attribute reader' do
+      backer = Backer.new("Avi")
+      expect(backer.name).to eq("Avi")
+    end
+
+
+
+
